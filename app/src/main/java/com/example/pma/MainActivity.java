24c8098fed4506c989455561c5d399e71fc46001
NikolaStankovic95 <stanee95@live.com>
2020-05-08 14:54:51 +0200

    Izgled settings aktivnosti

--- app/src/main/java/com/example/pma/MainActivity.java
+++ app/src/main/java/com/example/pma/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pma;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -58,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this,SettingsActivity.class));
             return true;
         }
 
